71b7fef4ce5796234ce15331ae7479a162b8b7f9
artur-hurynovich <artur.hurynovich@gmail.com>
2021-11-04 14:08:39 +0300

    ds-0015. Use EntityManager instead of JpaRepository; Remove unnecessary tests

--- src/main/java/com/hurynovich/data_storage/dao/impl/DataUnitSchemaDAOImpl.java
+++ src/main/java/com/hurynovich/data_storage/dao/impl/DataUnitSchemaDAOImpl.java
@@ -2,51 +2,89 @@ package com.hurynovich.data_storage.dao.impl;
 
 import com.hurynovich.data_storage.dao.DataUnitSchemaDAO;
 import com.hurynovich.data_storage.model.data_unit_schema.DataUnitSchemaEntity;
-import com.hurynovich.data_storage.repository.DataUnitSchemaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class DataUnitSchemaDAOImpl implements DataUnitSchemaDAO {
 
-	private final DataUnitSchemaRepository repository;
+	private final EntityManager entityManager;
 
-	public DataUnitSchemaDAOImpl(final @NonNull DataUnitSchemaRepository repository) {
-		this.repository = repository;
+	public DataUnitSchemaDAOImpl(final @NonNull EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 
 	@Override
 	public DataUnitSchemaEntity save(final @NonNull DataUnitSchemaEntity dataUnitSchema) {
-		return repository.save(dataUnitSchema);
+		if (dataUnitSchema.getId() == null) {
+			entityManager.persist(dataUnitSchema);
+		} else {
+			entityManager.merge(dataUnitSchema);
+		}
+
+		return dataUnitSchema;
 	}
 
 	@Override
 	public Optional<DataUnitSchemaEntity> findById(final @NonNull Long id) {
-		return repository.findById(id);
+		return Optional.ofNullable(entityManager.find(DataUnitSchemaEntity.class, id));
 	}
 
 	@Override
 	public List<DataUnitSchemaEntity> findAll() {
-		return repository.findAll();
+		final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<DataUnitSchemaEntity> criteriaQuery = criteriaBuilder.
+				createQuery(DataUnitSchemaEntity.class);
+		final Root<DataUnitSchemaEntity> root = criteriaQuery.from(DataUnitSchemaEntity.class);
+		criteriaQuery.select(root);
+
+		return entityManager.createQuery(criteriaQuery).getResultList();
 	}
 
 	@Override
 	public void deleteById(final @NonNull Long id) {
-		repository.deleteById(id);
+		final Optional<DataUnitSchemaEntity> dataUnitSchemaOptional = findById(id);
+		if (dataUnitSchemaOptional.isPresent()) {
+			entityManager.remove(dataUnitSchemaOptional.get());
+		} else {
+			throw new EntityNotFoundException("'DataUnitSchemaEntity' with id = " + id + " not found");
+		}
 	}
 
 
 	@Override
 	public boolean existsByName(final @NonNull String name) {
-		return repository.existsByName(name);
+		final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<DataUnitSchemaEntity> criteriaQuery = criteriaBuilder.
+				createQuery(DataUnitSchemaEntity.class);
+		final Root<DataUnitSchemaEntity> root = criteriaQuery.from(DataUnitSchemaEntity.class);
+		criteriaQuery.select(root).
+				where(criteriaBuilder.equal(root.get("name"), name));
+
+		return !entityManager.createQuery(criteriaQuery).getResultList().isEmpty();
 	}
 
 	@Override
 	public boolean existsByNameAndNotId(final @NonNull String name, final @NonNull Long id) {
-		return repository.existsByNameAndIdNot(name, id);
+		final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<DataUnitSchemaEntity> criteriaQuery = criteriaBuilder.
+				createQuery(DataUnitSchemaEntity.class);
+		final Root<DataUnitSchemaEntity> root = criteriaQuery.from(DataUnitSchemaEntity.class);
+		criteriaQuery.select(root).
+				where(criteriaBuilder.
+						and(
+								criteriaBuilder.equal(root.get("name"), name),
+								criteriaBuilder.notEqual(root.get("id"), id)));
+
+		return !entityManager.createQuery(criteriaQuery).getResultList().isEmpty();
 	}
 
 }
